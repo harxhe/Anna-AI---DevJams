ba950577de3bb7e6b0c040f31e60f6476802aa80
Harsh Vardhan <harshvardhan.p27@gmail.com>
2024-09-21 15:15:50 +0530

    Update groq-main.py
    imported data about academics buildings in vit and gave that data as a system prompt. added user input to get questions from the user

--- groq-main.py
+++ groq-main.py
@@ -2,6 +2,9 @@ GROQ_API_KEY = "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"
 
 from langchain_groq import ChatGroq
 
+with open('data.txt', 'r') as file:
+    text = file.read()
+
 llm = ChatGroq(
     model="mixtral-8x7b-32768",
     temperature=0.0,
@@ -9,9 +12,10 @@ llm = ChatGroq(
     api_key= "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"
 )
 
+user_input=input("Hello, Welcome to ANNA AI. How can I help you today?")
 messages = [
-    ("system", "You are a helpful bot named anna. Your task is to help your college juniors with their doubts.  "),
-    ("human", "Hello anna, what can you do  "),
+    ("system", f"You are a helpful bot named anna. Your task is to help your college juniors with their doubts. Here is some additional context from a file: {text} "),
+    ("human",  user_input),
 ]
 print(llm.invoke(messages).content)
 
